8f4f8e3c52da7daa91476f8b7bc7d91428d831ce
mcantillon21 <60069300+mcantillon21@users.noreply.github.com>
2023-03-31 18:09:02 -1000

    2nd stage change

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # print("Logs from your program will appear here!")
     
     command = sys.argv[3]
     args = sys.argv[4:]
