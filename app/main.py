ea1469e15045b83bc2fe7a297632ac05cceae7d7
mcantillon21 <60069300+mcantillon21@users.noreply.github.com>
2023-04-02 12:49:06 -1000

    6th stage

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ import tempfile
 import shutil
 import ctypes
 import sys
+from urllib import request
+import json
+import tarfile
+import requests
 
 def authenticate(image_name):
     url = f'https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/{image_name}:pull'
@@ -14,20 +18,25 @@ def authenticate(image_name):
 def fetch_manifest(image_name, token):
     url = f'https://registry.hub.docker.com/v2/library/{image_name}/manifests/latest'
     headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get(url, headers=headers)
-    manifest = response.json()
-    return manifest
+    req = request.Request(url, method="GET", headers=headers)
+    resp = request.urlopen(req)
+    resp = json.loads(resp.read().decode("utf-8"))
+    blobs = [layer["blobSum"] for layer in resp["fsLayers"]]
+    return blobs
 
-def pull_layers(image_name, manifest, chroot_dir):
-    layers = manifest['layers']
-    for layer in layers:
-        layer_hash = layer['digest']
-        layer_file = os.path.join(chroot_dir, f'{layer_hash}.tar')
-        url = f'https://registry.hub.docker.com/v2/library/{image_name}/blobs/{layer_hash}'
-        response = requests.get(url)
-        with open(layer_file, 'wb') as f:
-            f.write(response.content)
-        subprocess.run(['tar', '-C', chroot_dir, '-xvf', layer_file])
+def pull_layers(image_name, blobs, token, chroot_dir):
+    for blob in blobs:
+        url = f'https://registry.hub.docker.com/v2/library/{image_name}/blobs/{blob}'
+        headers = {'Authorization': f'Bearer {token}'}
+        req = request.Request(url, method="GET", headers=headers)
+        
+        with tempfile.TemporaryDirectory() as temp_dir:
+            with open(os.path.join(temp_dir, f"{blob}.tar"), "wb") as f:
+                with request.urlopen(req) as resp:
+                    f.write(resp.read())
+            for file in os.listdir(temp_dir):
+                ff = tarfile.open(os.path.join(temp_dir, file))
+                ff.extractall(chroot_dir)
 
 def run_command(chroot_dir, command):
     os.makedirs(os.path.join(chroot_dir, os.path.dirname(command).strip("/")))
@@ -46,11 +55,11 @@ def run_command(chroot_dir, command):
 def mydocker_run(image_name, command):
     chroot_dir = tempfile.mkdtemp()
     token = authenticate(image_name)
-    manifest = fetch_manifest(image_name, token)
-    pull_layers(image_name, manifest, chroot_dir)
+    blobs = fetch_manifest(image_name, token)
+    pull_layers(image_name, blobs, token, chroot_dir)
     run_command(chroot_dir, command)
 
 if __name__ == '__main__':
-    import requests
-    image_name, command = sys.argv[1], sys.argv[2:]
+    image_name, command, args = sys.argv[2], sys.argv[3], sys.argv[4:]
+    print(image_name, "IMAGE_NAME", command, "COMMAND")
     mydocker_run(image_name, command)
