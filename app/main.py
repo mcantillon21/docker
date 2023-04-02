9efc11aa5eb29c7cc5bb5c02a4f4db4b2ddcd881
mcantillon21 <60069300+mcantillon21@users.noreply.github.com>
2023-04-02 12:26:50 -1000

    6th stage

--- app/main.py
+++ app/main.py
@@ -1,63 +1,56 @@
 import requests
-import sys
+import os
+import subprocess
 import tempfile
 import shutil
-import os
-import tarfile
+import ctypes
+import sys
 
-def main():
+def authenticate(image_name):
+    url = f'https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/{image_name}:pull'
+    response = requests.get(url)
+    token = response.json()['access_token']
+    return token
 
-    # Parse command-line arguments
-    image_name = sys.argv[3]
-    command = sys.argv[4:]
-    
-    # Authenticate with Docker Hub
-    auth_endpoint = "https://auth.docker.io/token"
-    auth_data = {
-        "service": "registry.docker.io",
-        "scope": "repository:{image_name}:pull".format(image_name=image_name)
-    }
-    auth_headers = {
-        "Accept": "application/json"
-    }
-    auth_response = requests.get(auth_endpoint, params=auth_data, headers=auth_headers)
-    auth_token = auth_response.json()["token"]
-    print(auth_token)
-    
-    # Fetch image manifest
-    manifest_endpoint = "https://registry.hub.docker.com/v2/{image_name}/manifests/latest".format(image_name=image_name)
-    manifest_headers = {
-        "Authorization": "Bearer {auth_token}".format(auth_token=auth_token),
-        "Accept": "application/vnd.docker.distribution.manifest.v2+json"
-    }
-    manifest_response = requests.get(manifest_endpoint, headers=manifest_headers)
-    manifest_json = manifest_response.json()
-    
-    # Pull layers of image and extract to chroot directory
-    temp_dir = tempfile.mkdtemp()
-    layer_dir = os.path.join(temp_dir, "layers")
-    os.makedirs(layer_dir)
-    for layer in manifest_json["layers"]:
-        layer_digest = layer["digest"]
-        layer_endpoint = "https://registry.hub.docker.com/v2/{image_name}/blobs/{layer_digest}".format(
-            image_name=image_name, layer_digest=layer_digest)
-        layer_headers = {
-            "Authorization": "Bearer {auth_token}".format(auth_token=auth_token),
-            "Accept": "application/vnd.docker.image.rootfs.diff.tar.gzip"
-        }
-        layer_response = requests.get(layer_endpoint, headers=layer_headers, stream=True)
-        layer_tar = tarfile.open(fileobj=layer_response.raw, mode="r|gz")
-        layer_tar.extractall(layer_dir)
-        layer_tar.close()
-        
-    # Copy command into chroot directory
-    command_dir = os.path.join(temp_dir, "command")
-    os.makedirs(command_dir)
-    shutil.copy(command[0], os.path.join(command_dir, os.path.basename(command[0])))
-    
-    # Change root to chroot directory and execute command
-    os.chroot(temp_dir)
-    os.execv(os.path.join(command_dir, os.path.basename(command[0])), command)
+def fetch_manifest(image_name, token):
+    url = f'https://registry.hub.docker.com/v2/library/{image_name}/manifests/latest'
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(url, headers=headers)
+    manifest = response.json()
+    return manifest
 
-if __name__ == "__main__":
-    main()
+def pull_layers(image_name, manifest, chroot_dir):
+    layers = manifest['layers']
+    for layer in layers:
+        layer_hash = layer['digest']
+        layer_file = os.path.join(chroot_dir, f'{layer_hash}.tar')
+        url = f'https://registry.hub.docker.com/v2/library/{image_name}/blobs/{layer_hash}'
+        response = requests.get(url)
+        with open(layer_file, 'wb') as f:
+            f.write(response.content)
+        subprocess.run(['tar', '-C', chroot_dir, '-xvf', layer_file])
+
+def run_command(chroot_dir, command):
+    os.makedirs(os.path.join(chroot_dir, os.path.dirname(command).strip("/")))
+    shutil.copy(command, os.path.join(chroot_dir, command.strip("/")))
+    os.chroot(chroot_dir)
+
+    # Use ctypes to call the unshare() function from the C library with the CLONE_NEWPID flag
+    libc = ctypes.CDLL(None)
+    CLONE_NEWPID = 0x20000000
+    libc.unshare.argtypes = [ctypes.c_int]
+    libc.unshare(CLONE_NEWPID)
+
+    completed_process = subprocess.run([command])
+    exit(completed_process.returncode)
+
+def mydocker_run(image_name, command):
+    chroot_dir = tempfile.mkdtemp()
+    token = authenticate(image_name)
+    manifest = fetch_manifest(image_name, token)
+    pull_layers(image_name, manifest, chroot_dir)
+    run_command(chroot_dir, command)
+
+if __name__ == '__main__':
+    image_name, command = sys.argv[1], sys.argv[2:]
+    mydocker_run(image_name, command)
